9638e0f13d2d4b298d608dbaf41c3958ee8a1500
futoase <futoase@gmail.com>
2013-10-16 20:22:54 +0900

    Modified: Improve syntax. (use go fmt)

--- api.go
+++ api.go
@@ -1,33 +1,33 @@
 package td
 
 import (
-  "strconv"
+	"strconv"
 )
 
 func (t *TreasureData) ListDatabases() string {
-  return GetRequest(t.Options, "/v3/database/list")
+	return GetRequest(t.Options, "/v3/database/list")
 }
 
 func (t *TreasureData) ListTables(db string) string {
-  return GetRequest(t.Options, "/v3/table/list/" + db)
+	return GetRequest(t.Options, "/v3/table/list/"+db)
 }
 
 func (t *TreasureData) ShowJobs(job_id int) string {
-  return GetRequest(t.Options, "/v3/job/show/" + strconv.Itoa(job_id))
+	return GetRequest(t.Options, "/v3/job/show/"+strconv.Itoa(job_id))
 }
 
 func (t *TreasureData) JobResult(job_id int) string {
-  return GetRequest(t.Options, "/v3/job/result" + strconv.Itoa(job_id))
+	return GetRequest(t.Options, "/v3/job/result"+strconv.Itoa(job_id))
 }
 
 func (t *TreasureData) ListSchedules() string {
-  return GetRequest(t.Options, "/v3/schedule/list")
+	return GetRequest(t.Options, "/v3/schedule/list")
 }
 
 func (t *TreasureData) ListResult() string {
-  return GetRequest(t.Options, "/v3/result/list")
+	return GetRequest(t.Options, "/v3/result/list")
 }
 
 func (t *TreasureData) ServerStatus() string {
-  return GetRequest(t.Options, "/v3/system/server_status")
+	return GetRequest(t.Options, "/v3/system/server_status")
 }
--- td.go
+++ td.go
@@ -1,31 +1,31 @@
 package td
 
 type Options struct {
-  Header string
-  Host string
-  ApiKey string
-  Protocol string
+	Header   string
+	Host     string
+	ApiKey   string
+	Protocol string
 }
 
 type TreasureData struct {
-  Options *Options
+	Options *Options
 }
 
 func NewOptions(apikey string) *Options {
-  if len(apikey) == 0 {
-    return nil
-  }
+	if len(apikey) == 0 {
+		return nil
+	}
 
-  o := new(Options)
-  o.ApiKey = apikey
-  o.Header = "AUTHORIZATION"
-  o.Host = "api.treasure-data.com"
-  o.Protocol = "http"
-  return o
+	o := new(Options)
+	o.ApiKey = apikey
+	o.Header = "AUTHORIZATION"
+	o.Host = "api.treasure-data.com"
+	o.Protocol = "http"
+	return o
 }
 
-func NewTreasureData (o *Options) *TreasureData {
-  t := new(TreasureData)
-  t.Options = o
-  return t
+func NewTreasureData(o *Options) *TreasureData {
+	t := new(TreasureData)
+	t.Options = o
+	return t
 }
