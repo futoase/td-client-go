408126bd3c05a3b0783319ab7a98ca741e6d2fe4
futoase <futoase@gmail.com>
2013-10-28 21:56:23 +0900

    Modified: Repair file contents.

--- examples/list_tables.go
+++ examples/list_tables.go
@@ -15,8 +15,8 @@ func main() {
 	client := td.NewTreasureData(options)
 	d := client.ListDatabases()
 
-	for i := 0; i < len(d.Databases); i++ {
-		row := d.Databases[i]
-		fmt.Printf("table: %v\n count: %v\n created_at: %v\n updated_at: %v\n", row.Name, row.Count, row.Created_At, row.Updated_At)
+	t := client.ListTables(d.Databases[0].Name)
+	for i := 0; i < len(t.Tables); i++ {
+		fmt.Printf("id: %v, name: %v, estimated_storate_size: %v, counter_updated_at: %v, last_log_timestamp: %v, expire_days: %v, type: %v, count: %v, created_at: %v, updated_at: %v, schema: %v", t.Tables[i].Id, t.Tables[i].Name, t.Tables[i].Estimated_Storage_Size, t.Tables[i].Counter_Updated_At, t.Tables[i].Expire_Days, t.Tables[i].Type, t.Tables[i].Count, t.Tables[i].Created_At, t.Tables[i].Updated_At, t.Tables[i].Schema)
 	}
 }
