b1f5e8d9babaae592632719eb4cefb8ab6ed1593
futoase <futoase@gmail.com>
2013-11-06 18:19:25 +0900

    Modified: Added comment.

--- api_result.go
+++ api_result.go
@@ -5,125 +5,125 @@ type DatabaseList struct {
 }
 
 type Database struct {
-	Name         string
-	Count        int
-	Created_At   string
-	Updated_At   string
-	Organization string
+	Name         string `json:"name"`
+	Count        int    `json:"count"`
+	Created_At   string `json:"created_at"`
+	Updated_At   string `json:"updated_at"`
+	Organization string `json:"organization"`
 }
 
 type TableList struct {
-	Database string
-	Tables   []Table
+	Database string  `json:"database"`
+	Tables   []Table `json:"tables"`
 }
 
 type Table struct {
-	Id                     int
-	Name                   string
-	Estimated_Storage_Size int
-	Counter_Updated_At     string
-	Last_Log_Timestamp     string
-	Expire_Days            string
-	Type                   string
-	Count                  int
-	Created_At             string
-	Updated_At             string
-	Schema                 string
+	Id                     int    `json:"id"`
+	Name                   string `json:"name"`
+	Estimated_Storage_Size int    `json:"estimated_storage_size"`
+	Counter_Updated_At     string `json:"counter_updated_at"`
+	Last_Log_Timestamp     string `json:"last_log_timestamp"`
+	Expire_Days            string `json:"expire_days"`
+	Type                   string `json:"type"`
+	Count                  int    `json:"count"`
+	Created_At             string `json:"created_at"`
+	Updated_At             string `json:"updated_at"`
+	Schema                 string `json:"schema"`
 }
 
 type JobsList struct {
-	Count int
-	From  int
-	To    int
-	Jobs  []Job
+	Count int   `json:"count"`
+	From  int   `json:"from"`
+	To    int   `json:"to"`
+	Jobs  []Job `json:"jobs"`
 }
 
 type Job struct {
-	Status             string
-	Job_Id             string
-	Created_At         string
-	Updated_At         string
-	Start_At           string
-	End_At             string
-	Query              string
-	Type               string
-	Priority           int
-	Retry_Limit        int
-	Hive_Result_Schema string
-	Result             string
-	Url                string
-	User_Name          string
-	Organization       string
-	Database           string
+	Status             string `json:"status"`
+	Job_Id             string `json:"job_id"`
+	Created_At         string `json:"created_at"`
+	Updated_At         string `json:"updated_at"`
+	Start_At           string `json:"start_at"`
+	End_At             string `json:"end_at"`
+	Query              string `json:"query"`
+	Type               string `json:"type"`
+	Priority           int    `json:"priority"`
+	Retry_Limit        int    `json:"retry_limit"`
+	Hive_Result_Schema string `json:"hive_result_schema"`
+	Result             string `json:"result"`
+	Url                string `json:"url"`
+	User_Name          string `json:"user_name"`
+	Organization       string `json:"organization"`
+	Database           string `json:"database"`
 }
 
 type JobResults struct {
-	JobResult []JobResult
+	JobResult []JobResult `json:"job_result"`
 }
 
 type JobResult struct {
-	Status string
-	Count  string
+	Status string `json:"status"`
+	Count  string `json:"count"`
 }
 
 type Schedules struct {
-	Schedules []Schedule
+	Schedules []Schedule `json:"schedules"`
 }
 
 type Schedule struct {
-	Name        string
-	Cron        string
-	Type        string
-	Query       string
-	TimeZone    string
-	Delay       int
-	Database    string
-	User_Name   string
-	Priority    int
-	Retry_Limit int
-	Result      string
-	Next_Time   string
+	Name        string `json:"name"`
+	Cron        string `json:"cron"`
+	Type        string `json:"type"`
+	Query       string `json:"query"`
+	TimeZone    string `json:"timezone"`
+	Delay       int    `json:"delay"`
+	Database    string `json:"database"`
+	User_Name   string `json:"user_name"`
+	Priority    int    `json:"prioriby"`
+	Retry_Limit int    `json:"retry_limit"`
+	Result      string `json:"result"`
+	Next_Time   string `json:"next_time"`
 }
 
 type ServerStatus struct {
-	Status string
+	Status string `json:"status"`
 }
 
 type Results struct {
-	Result []Result
+	Result []Result `json:"result"`
 }
 
 type Result struct {
-	Name         string
-	Url          string
-	Organization string
+	Name         string `json:"name"`
+	Url          string `json:"url"`
+	Organization string `json:"organization"`
 }
 
 type CreateTableResult struct {
-	Table    string
-	Type     string
-	Database string
-	Message  string
+	Table    string `json:"table"`
+	Type     string `json:"type"`
+	Database string `json:"database"`
+	Message  string `json:"message"`
 }
 
 type SwapTableResult struct {
-	Table1   string
-	Table2   string
-	Database string
-	Message  string
+	Table1   string `json:"table1"`
+	Table2   string `json:"table2"`
+	Database string `json:"database"`
+	Message  string `json:"message"`
 }
 
 type CreateDatabaseResult struct {
-	Database string
-	Message  string
+	Database string `json:"database"`
+	Message  string `json:"message"`
 }
 
 type DeleteDatabaseResult struct {
-	Database string
-	Message  string
+	Database string `json:"database"`
+	Message  string `json:"message"`
 }
 
 type KillJobResult struct {
-	Job_Id        string
-	Former_Status string
+	Job_Id        string `json:"job_id"`
+	Former_Status string `json:"former_status"`
 }
